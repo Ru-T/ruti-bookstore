61289827dd8433be5bf1379ef3aaafa77300b498
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-27 13:31:07 -0500

    Add show action to controller, routes, and view for book.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,15 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show]
+
   def index
-    @books = Book.all.paginate(page: params[:page])
+    @books = Book.all.paginate(page: params[:page], per_page: 25)
   end
 
   private
 
+  def set_book
+    @book = Book.find(params[:id])
+  end
   # strong params
   def user_params
     params.require(:book).permit(
