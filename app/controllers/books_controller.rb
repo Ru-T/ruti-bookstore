e527d28eaab5ce4b992fd4587404699fdccb249d
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-27 14:46:39 -0500

    Make columns sortable.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,9 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show]
+  helper_method :sort_column, :sort_direction
 
   def index
-    @books = Book.all.paginate(page: params[:page], per_page: 25)
+    @books = Book.order("#{sort_column} #{sort_direction}").paginate(page: params[:page], per_page: 25)
   end
 
   private
@@ -20,5 +21,17 @@ class BooksController < ApplicationController
       :category,
       :description
     )
+  end
+
+  def sortable_columns
+    ["title", "published_date", "author", "price", "category"]
+  end
+
+  def sort_column
+    sortable_columns.include?(params[:column]) ? params[:column] : "title"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
