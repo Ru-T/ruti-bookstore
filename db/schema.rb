6729d5f54561eb9178f2f80dcaca0e9b3958c3e5
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-27 09:37:18 -0500

    Add confirmable to users table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160127142132) do
+ActiveRecord::Schema.define(version: 20160126222557) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -35,11 +35,11 @@ ActiveRecord::Schema.define(version: 20160127142132) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
