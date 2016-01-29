43b1a8223e568c6bbba773a50aba011d551e7799
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-01-29 13:36:34 -0500

    Create feature and steps file for integration tests.

---
+++ features/purchase.feature
@@ -0,0 +1,44 @@
+Feature: Purchasing a Book
+  Scenario: Happy Path
+    Given I am logged into the site
+    And there are 100 books in the database
+    When I visit the site root path
+    And I click on a book
+    And I click the "Add to Cart" button
+    Then the book is added to my cart
+    When I visit my cart
+    Then I see the book in my cart
+    When I click the "Purchase" button
+    Then I am asked for my shipping address
+    When I enter my shipping address
+    Then I am asked for my billing address
+    When I enter my billing address
+    Then I am asked for my credit card
+    When I enter my credit card
+    And I click the "Submit Order" button
+    Then I am asked to review the order total
+    When I click the "Confirm" button
+    Then I am shown the order summary
+    Then my credit card is saved for future purchases
+    And I am emailed an order invoice containing the books details, quantity, subtotal, and order total
+
+  Scenario: With a saved Credit Card
+    Given I am logged into the site
+    And there are 100 books in the database
+    And I have a credit card saved on the site
+    When I visit the site root path
+    And I click on a book
+    And I click the "Add to Cart" button
+    Then the book is added to my cart
+    When I visit my cart
+    Then I see the book in my cart
+    When I click the "Purchase" button
+    Then I am asked for my shipping address
+    When I enter my shipping address
+    Then I am asked if I want to use my already saved credit card
+    When I confirm using my saved credit card
+    Then I am asked to review the order total
+    When I click the "Confirm" button
+    Then I am shown the order summary
+    And my credit card is saved for future purchases
+    And I am emailed an order invoice containing the books details, quantity, subtotal, and order total
---
+++ features/step_definitions/purchase_steps.rb
@@ -0,0 +1,67 @@
+When(/^I click on a book$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the book is added to my cart$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I visit my cart$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I see the book in my cart$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I am asked for my shipping address$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I enter my shipping address$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I am asked for my billing address$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I enter my billing address$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I am asked for my credit card$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I enter my credit card$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I am asked to review the order total$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I am shown the order summary$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^my credit card is saved for future purchases$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I am emailed an order invoice containing the books details, quantity, subtotal, and order total$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I have a credit card saved on the site$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I am asked if I want to use my already saved credit card$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I confirm using my saved credit card$/) do
+  pending # express the regexp above with the code you wish you had
+end
