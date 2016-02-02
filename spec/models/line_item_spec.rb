22fd74ab488bcec94c0149cd80549a1dc8ad2e9c
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-02 15:47:33 -0500

    Add test coverage.

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe LineItem, type: :model do
   let(:user) { create(:user) }
   let(:book) { create(:book) }
-  let(:line_item) { create(:line_item, book: book, quantity: 3 }
+  let(:line_item) { create(:line_item, book: book, quantity: 3) }
+  let(:line_item2) { create(:line_item, active: false) }
 
   describe "validations" do
     it "is valid without active flag" do
@@ -26,6 +27,12 @@ RSpec.describe LineItem, type: :model do
   describe "#total_price" do
     it "returns item price multiplied by line item quantity" do
       expect(line_item.total_price).to eq 600
+    end
+  end
+
+  describe ".active" do
+    it "returns only active line items" do
+      expect(LineItem.active).to_not include line_item2
     end
   end
 end
