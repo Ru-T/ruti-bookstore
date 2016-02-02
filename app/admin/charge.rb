c54f3af5de4e320f9252f8c97a6c4f40e0546211
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-02 11:36:54 -0500

    Add file for admin view of charges.

---
+++ app/admin/charge.rb
@@ -0,0 +1,13 @@
+ActiveAdmin.register Stripe::Charge do
+
+  index do
+    column :customer
+    column :amount
+    column :description
+    column :currency
+    column :receipt_email
+    actions
+  end
+
+  permit_params :customer, :amount, :description, :currency, :receipt_email
+end
