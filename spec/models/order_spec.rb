a9da3ed76c6c508409813163aede78ef522e3fa9
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-03 15:06:08 -0500

    Test coverage for purchase method on order.

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
   let(:user) { create(:user) }
-  let(:book) { create(:book) }
   let(:cart) { create(:cart, user: user) }
+  let(:book) { create(:book) }
   let(:line_item) { create(:line_item, cart: cart, book: book) }
   let(:order) { create(:order, user: user) }
 
@@ -11,6 +11,7 @@ RSpec.describe Order, type: :model do
     it "assigns cart's line items to order when order is created" do
       order.purchase_line_items
       order.reload
+      expect(cart.line_items).to be_empty
       expect(order.line_items).to include line_item
     end
   end
