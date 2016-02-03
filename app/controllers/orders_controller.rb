5e56df35ab6d2f9e103b02ab0bf3357abe78fba2
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-02 22:09:37 -0500

    Add credit card fields to new purchase order form.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,42 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_order, only: [:show]
 
   def create
-    amount_in_cents = cart.total_cart_price
+    @order = Order.new(order_params)
+    redirect_to @order, notice: "Your order has been completed"
+  end
+  # def create
+  #   amount_in_cents = cart.total_cart_price
+  #
+  #   customer = Stripe::Customer.create(
+  #     email:  params[:stripeEmail],
+  #     source: params[:stripeToken]
+  #   )
+  #
+  #   charge = Stripe::Charge.create(
+  #     customer:    customer.id,
+  #     amount:      amount_in_cents,
+  #     description: 'Bookstore purchase',
+  #     currency:    'usd',
+  #     receipt_email: customer.email
+  #   )
+  #
+  #   flash[:notice] = "Successfully created a charge"
+  #   redirect_to pending_purchases_path
+  # end
 
-    customer = Stripe::Customer.create(
-      email:  params[:stripeEmail],
-      source: params[:stripeToken]
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
+  end
+  # strong params
+  def order_params
+    params.require(:order).permit(
+      :user_id,
+      :order_status,
+      :total
     )
-
-    charge = Stripe::Charge.create(
-      customer:    customer.id,
-      amount:      amount_in_cents,
-      description: 'Bookstore purchase',
-      currency:    'usd',
-      receipt_email: customer.email
-    )
-
-    flash[:notice] = "Successfully created a charge"
-    redirect_to pending_purchases_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   resources :books, only: [:index, :show]
   devise_for :users, controllers: { registrations: "registrations" }
-  resources :orders, only: [:create, :new]
+  resources :orders, only: [:create, :new, :show]
   resources :carts, only: [:show]
   resources :line_items, except: [:new]
 end
