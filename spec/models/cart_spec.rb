ebf89dbecf0adefbc07ab7dfc776fb87a2e7d818
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-02 19:47:48 -0500

    Test coverage for total cart price method.

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  let(:cart) { create(:cart) }
-  let(:book) { create(:book) }
-  let(:line_item) { create(:line_item, book: book, user: cart.user, quantity: 1, active: true) }
+  let(:user) { create(:user) }
+  let(:cart) { create(:cart, user: user) }
+  let(:book) { create(:book, price: 200) }
+  let(:book2) { create(:book, price: 500) }
 
   describe "#total_price" do
     it "returns the total of all line items" do
-      expect(cart.total_cart_price).to eq 200
+      user.add_to_cart(book)
+      user.add_to_cart(book2)
+      expect(cart.total_cart_price).to eq 700
     end
   end
 end
