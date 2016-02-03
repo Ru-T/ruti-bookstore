7e667f3d510c90a23c94da3b5c1f8354929af9df
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-03 13:11:03 -0500

    Add test for cart total price method.

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:cart) { create(:cart, user: user) }
+  let(:book) { create(:book, price: 200) }
+  let(:book2) { create(:book, price: 500) }
+
+  describe "#total_price" do
+    it "returns the total of all line items" do
+      user.add_to_cart(book)
+      user.add_to_cart(book2)
+      expect(cart.total_cart_price).to eq 700
+    end
+  end
 end
