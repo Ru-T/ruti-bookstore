4da9d81953ae92b65b71cee055f17ab6726ae400
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-03 14:05:29 -0500

    Create order model and add order_id to line items.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160203173550) do
+ActiveRecord::Schema.define(version: 20160203190453) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -67,6 +67,14 @@ ActiveRecord::Schema.define(version: 20160203173550) do
     t.integer  "cart_id"
     t.integer  "book_id"
     t.integer  "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/factories/orders.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :order do
+    user_id 1
+    total 1
+  end
+end
