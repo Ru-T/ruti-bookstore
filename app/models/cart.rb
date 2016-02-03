65038020b3d9c12fb555e8adf2533c875ed09cd3
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-03 13:47:21 -0500

    Remove redundant self.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -7,14 +7,14 @@ class Cart < ActiveRecord::Base
   end
 
   def add_to_cart(book)
-    self.line_items.create!(book_id: book.id, quantity: 1)
+    line_items.create!(book_id: book.id, quantity: 1)
   end
 
   def remove_from_cart(book)
-    self.line_items.find_by_book_id(book.id).destroy
+    line_items.find_by_book_id(book.id).destroy
   end
 
   def in_cart?(book)
-    self.line_items.any? { |line_item| line_item[:book_id] == book.id }
+    line_items.any? { |line_item| line_item[:book_id] == book.id }
   end
 end
