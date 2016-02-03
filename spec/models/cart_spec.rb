ee0b90cb7d7891c6ce816a20dc5f829936b0e106
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-03 11:47:04 -0500

    Move test coverage from cart to order for total price.

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -5,12 +5,4 @@ RSpec.describe Cart, type: :model do
   let(:cart) { create(:cart, user: user) }
   let(:book) { create(:book, price: 200) }
   let(:book2) { create(:book, price: 500) }
-
-  describe "#total_price" do
-    it "returns the total of all line items" do
-      user.add_to_cart(book)
-      user.add_to_cart(book2)
-      expect(cart.total_cart_price).to eq 700
-    end
-  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { create(:user) }
   let(:book) { create(:book) }
+  let(:line_item) { create(:line_item, book: book) }
 
   describe "#remove_from_cart" do
     it "allows a user to remove a book from his/her cart" do
-      user.add_to_cart(book)
+      user.line_items << line_item
       user.remove_from_cart(book)
       user.reload
       expect(user.line_items).to be_empty
@@ -15,8 +16,7 @@ RSpec.describe User, type: :model do
 
   describe "#in_cart?" do
     it "checks if a book is already in a user's cart" do
-      user.add_to_cart(book)
-      expect(user.in_cart?(book)).to eq true
+      user.line_items << line_item
       user.remove_from_cart(book)
       user.reload
       expect(user.in_cart?(book)).to eq false
