0d3c8713a85eb2587acd2c18e5c4ec51998b466d
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-04 08:49:21 -0500

    Integrate suggestions from PR review.

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,6 @@ class Order < ActiveRecord::Base
   has_many :line_items
 
   def purchase_line_items
-    LineItem.where(cart: user.cart).update_all(cart_id: nil, order_id: id)
+    user.cart.line_items.update_all(cart_id: nil, order_id: id)
   end
 end
