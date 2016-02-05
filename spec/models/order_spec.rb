e119f8312c5c9301e29d5f8f5d0a00f89d0366b9
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-05 14:24:50 -0500

    Add test for save_with_payment method.

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'stripe_mock'
 
 RSpec.describe Order, type: :model do
   let(:user) { create(:user) }
@@ -6,6 +7,10 @@ RSpec.describe Order, type: :model do
   let(:book) { create(:book) }
   let(:line_item) { create(:line_item, cart: cart, book: book, order: nil) }
   let(:order) { create(:order, user: user) }
+  let(:stripe_helper) { StripeMock.create_test_helper }
+
+  before { StripeMock.start }
+  after { StripeMock.stop }
 
   describe "#purchase_line_items" do
     it "assigns cart's line items to order when order is created" do
@@ -13,6 +18,14 @@ RSpec.describe Order, type: :model do
       order.purchase_line_items
       expect(order.reload.line_items).to include line_item
       expect(cart.reload.line_items).to be_empty
+    end
+  end
+
+  describe "#save_with_payment" do
+    it "creates an order with Stripe" do
+      expect do
+        order = Order.create(user: user, stripe_token: stripe_helper.generate_card_token)
+      end.to change{Order.count}.by(1)
     end
   end
 end
