8942549742bccc2263a22a6125ca0748d5a15b99
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-04 21:42:03 -0500

    Remove unused preview file.

--- spec/factories/credit_cards.rb
+++ spec/factories/credit_cards.rb
@@ -1,11 +1,10 @@
 FactoryGirl.define do
   factory :credit_card do
-    user_id 1
-    last_four_digits 1
+    last_four_digits 1234
     billing_address1 "MyString"
     billing_address2 "MyString"
     billing_city "MyString"
     billing_state "MyString"
-    billing_zip 1
+    billing_zip 27701
   end
 end
