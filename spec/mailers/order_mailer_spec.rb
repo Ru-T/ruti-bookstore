c9d8b6953e62402733246811355f329573857c01
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-04 21:46:41 -0500

    Add test coverage

--- spec/mailers/order_mailer_spec.rb
+++ spec/mailers/order_mailer_spec.rb
@@ -1,5 +1,22 @@
 require "rails_helper"
 
 RSpec.describe OrderMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:book) { create(:book) }
+  let(:cart) { create(:cart, user: user) }
+  let(:order) { create(:order, user: user) }
+  let(:line_item) { create(:line_item, cart: cart, book: book, order: nil) }
+
+  describe "send order receipt to correct user" do
+    it "sends order receipt to correct user" do
+      order.purchase_line_items
+
+      receipt = OrderMailer.receipt_email(user).deliver_now
+      expect(ActionMailer::Base.deliveries).to_not be_empty
+
+      expect(receipt.from).to eq ["receipt@bookstore.com"]
+      expect(receipt.to).to eq [user.email]
+      expect(receipt.subject).to eq "Your Order Has Been Completed"
+    end
+  end
 end
