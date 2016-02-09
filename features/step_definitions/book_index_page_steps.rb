89323f378063b5ac9d1fb8beef17b045e8e5772f
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-09 14:28:24 -0500

    Pass sorting feature specs.

--- features/step_definitions/book_index_page_steps.rb
+++ features/step_definitions/book_index_page_steps.rb
@@ -1,6 +1,6 @@
 Given(/^there are 100 books in the database$/) do
-  @book = FactoryGirl.create(:book, published_date: Date.today - 2.days, order_count: 2)
-  @book1 = FactoryGirl.create(:book, published_date: Date.today, order_count: 1)
+  FactoryGirl.create(:book, published_date: Date.today, order_count: 2)
+  FactoryGirl.create(:book, order_count: 1)
   FactoryGirl.create_list(:book, 98)
 end
 
@@ -9,8 +9,7 @@ Then(/^I see a list of books in the database$/) do
 end
 
 Then(/^the books are ordered by published date$/) do
-  expect(Book.first).to eq @book1
-  expect(Book.last).to eq Book.order(published_date: :desc).last
+  expect(Book.first).to eq Book.order(published_date: :desc).first
 end
 
 Then(/^the list of 100 books are paginated in pages of 25 books per page$/) do
@@ -32,7 +31,7 @@ Then(/^I am shown a list of books with that title$/) do
 end
 
 Given(/^some books have been ordered$/) do
-  @books = Book.order("published_date DESC")
+  @books = Book.order(published_date: :desc)
 end
 
 When(/^I sort by "([^"]*)"$/) do |sort|
@@ -40,7 +39,6 @@ When(/^I sort by "([^"]*)"$/) do |sort|
 end
 
 Then(/^the books are re\-sorted based on the amount of times they are purchased$/) do
-  Book.order("order_count DESC")
-  expect(Book.first).to eq @book
-  expect(Book.last).to eq Book.order(order_count: :desc).last
+  Book.order(order_count: :desc)
+  expect(Book.first).to eq Book.order(order_count: :desc).first
 end
