802bc8b460af9a1fc0d6cfc2af9937f3da2ec51d
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-10 12:49:19 -0500

    Show prices in dollars on orders.

--- app/admin/book.rb
+++ app/admin/book.rb
@@ -4,16 +4,8 @@ ActiveAdmin.register Book do
     column :title
     column :author
     column :published_date
-    column :price do |book|
-      div :class => "price" do
-        number_to_currency book.price
-      end
-    end
-    column :discount do |book|
-      div :class => "discount" do
-        number_to_currency book.discount
-      end
-    end
+    column(:price) { |book| number_to_currency book.price }
+    column(:discount) { |book| number_to_currency book.discount }
     column :category
     column :description
     actions
--- app/admin/order.rb
+++ app/admin/order.rb
@@ -6,8 +6,9 @@ ActiveAdmin.register Order do
       table_for order.line_items do
         column :book
         column :quantity
-        column(:price) { |line_item| line_item.book.price }
-        column :total_price
+        column(:price) { |line_item| number_to_currency line_item.book.price }
+        column(:discount_price) { |line_item| number_to_currency line_item.book.discount_price }
+        column (:total_price) { |order| number_to_currency order.total_price }
         column :shipping_city
         column :shipping_zip
       end
