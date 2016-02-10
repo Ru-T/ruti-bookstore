f802f9c79923507a838dae87b8ca04fabc4b7362
Ruti Wajnberg <ruti.wajnberg@smashingboxes.com>
2016-02-10 12:45:05 -0500

    Display price and discount in admin in $, not cents.

--- app/admin/book.rb
+++ app/admin/book.rb
@@ -4,8 +4,16 @@ ActiveAdmin.register Book do
     column :title
     column :author
     column :published_date
-    column :price_cents
-    column :discount_cents
+    column :price do |book|
+      div :class => "price" do
+        number_to_currency book.price
+      end
+    end
+    column :discount do |book|
+      div :class => "discount" do
+        number_to_currency book.discount
+      end
+    end
     column :category
     column :description
     actions
